6cfe5e5ee5771fed7b509be0b6b451aefe213ac9
itaji-create <itaji.1999@gmail.com>
2022-09-29 10:26:07 -0300

    test: teste do relatório individual do produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,16 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "farinha",
+        "Farinini",
+        "01-05-2021",
+        "02-06-2023",
+        "BR85 2004 2335",
+        "ao abrigo de luz"
+    )
+    f = "O produto farinha fabricado em 01-05-2021 por Farinini "
+    f2 = "com validade até 02-06-2023 precisa ser armazenado ao abrigo de luz."
+    assert str(product) == f + f2
