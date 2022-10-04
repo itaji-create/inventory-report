1b55b858082662616d4b47b7598b53914c60f095
itaji-create <itaji.1999@gmail.com>
2022-10-04 10:29:16 -0300

    feat: gera arquivos através de arquivos xml, json e csv

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,39 @@
+import csv
+import json
+import xmltodict
+
+from ..reports.simple_report import SimpleReport
+from ..reports.complete_report import CompleteReport
+
+
+class Inventory:
+    def csv_reader(path):
+        with open(path) as file:
+            reader = list(csv.DictReader(file))
+            return reader
+
+    def json_reader(path):
+        with open(path) as file:
+            reader = json.load(file)
+            return reader
+
+    def xml_reader(path):
+        with open(path) as file:
+            reader = xmltodict.parse(file.read())["dataset"]["record"]
+            return list(reader)
+
+    @classmethod
+    def import_data(self, path, report):
+        if "csv" in path:
+            report_data = self.csv_reader(path)
+
+        if "json" in path:
+            report_data = self.json_reader(path)
+
+        if "xml" in path:
+            report_data = self.xml_reader(path)
+
+        if report == "simples":
+            return SimpleReport.generate(report_data)
+
+        return CompleteReport.generate(report_data)
--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -6,9 +6,9 @@ class CompleteReport(SimpleReport):
     @classmethod
     def generate(self, params):
         simple_report = super().generate(params)
-        empresas = [item["nome_da_empresa"] for item in params]
+        companies = [item["nome_da_empresa"] for item in params]
 
-        company_most_common = Counter(empresas).most_common()
+        company_most_common = Counter(companies).most_common()
         return (
             f"{simple_report}\n"
             f"Produtos estocados por empresa:\n"
