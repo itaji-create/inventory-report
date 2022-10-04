d2138da73f013e2fbbad173793f20106d476d513
itaji-create <itaji.1999@gmail.com>
2022-10-04 12:06:30 -0300

    feat: descomentar arquivo inventory

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,39 +1,39 @@
-# import csv
-# import json
-# import xmltodict
+import csv
+import json
+import xmltodict
 
-# from inventory_report.reports.simple_report import SimpleReport
-# from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
 
 
-# class Inventory:
-#     def csv_reader(path):
-#         with open(path) as file:
-#             reader = list(csv.DictReader(file))
-#             return reader
+class Inventory:
+    def csv_reader(path):
+        with open(path) as file:
+            reader = list(csv.DictReader(file))
+            return reader
 
-#     def json_reader(path):
-#         with open(path) as file:
-#             reader = json.load(file)
-#             return reader
+    def json_reader(path):
+        with open(path) as file:
+            reader = json.load(file)
+            return reader
 
-#     def xml_reader(path):
-#         with open(path) as file:
-#             reader = xmltodict.parse(file.read())["dataset"]["record"]
-#             return list(reader)
+    def xml_reader(path):
+        with open(path) as file:
+            reader = xmltodict.parse(file.read())["dataset"]["record"]
+            return list(reader)
 
-#     @classmethod
-#     def import_data(self, path, report):
-#         if "csv" in path:
-#             report_data = self.csv_reader(path)
+    @classmethod
+    def import_data(self, path, report):
+        if "csv" in path:
+            report_data = self.csv_reader(path)
 
-#         if "json" in path:
-#             report_data = self.json_reader(path)
+        if "json" in path:
+            report_data = self.json_reader(path)
 
-#         if "xml" in path:
-#             report_data = self.xml_reader(path)
+        if "xml" in path:
+            report_data = self.xml_reader(path)
 
-#         if report == "simples":
-#             return SimpleReport.generate(report_data)
+        if report == "simples":
+            return SimpleReport.generate(report_data)
 
-#         return CompleteReport.generate(report_data)
+        return CompleteReport.generate(report_data)
