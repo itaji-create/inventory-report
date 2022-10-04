e3010c42d247a2a642820e9c2829dcf80dc153f5
itaji-create <itaji.1999@gmail.com>
2022-10-04 12:02:50 -0300

    .

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -6,12 +6,12 @@ class SimpleReport:
     def generate(self, params):
         datas_fabricacao = [item["data_de_fabricacao"] for item in params]
         datas_validade = [item["data_de_validade"] for item in params]
-        empresas = [item["nome_da_empresa"] for item in params]
+        companies = [item["nome_da_empresa"] for item in params]
 
-        empresa_most_common = Counter(empresas).most_common(1)[0][0]
+        company_most_common = Counter(companies).most_common(1)[0][0]
 
         return (
             f"Data de fabricação mais antiga: {min(datas_fabricacao)}\n"
             f"Data de validade mais próxima: {min(datas_validade)}\n"
-            f"Empresa com mais produtos: {empresa_most_common}"
+            f"Empresa com mais produtos: {company_most_common}"
         )
