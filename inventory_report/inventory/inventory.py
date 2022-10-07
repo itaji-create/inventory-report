64b962f03397aba0761d8fb5c17c0d079d43ec04
itaji-create <itaji.1999@gmail.com>
2022-10-07 11:01:11 -0300

    refactor: adiciona classes importer ao inventory

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,42 +1,22 @@
-import csv
-import json
-import xmltodict
-
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
-# from inventory_report.importer.importer import Importer
-# from inventory_report.importer.csv_importer import CsvImporter
-# from inventory_report.importer.json_importer import JsonImporter
-# from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
 
 
 class Inventory:
-    def csv_reader(path):
-        with open(path) as file:
-            reader = list(csv.DictReader(file))
-            return reader
-
-    def json_reader(path):
-        with open(path) as file:
-            reader = json.load(file)
-            return reader
-
-    def xml_reader(path):
-        with open(path) as file:
-            reader = xmltodict.parse(file.read())["dataset"]["record"]
-            return list(reader)
-
     @classmethod
     def import_data(self, path, report):
         if "csv" in path:
-            report_data = self.csv_reader(path)
+            report_data = CsvImporter.import_data(path)
 
         if "json" in path:
-            report_data = self.json_reader(path)
+            report_data = JsonImporter.import_data(path)
 
         if "xml" in path:
-            report_data = self.xml_reader(path)
+            report_data = XmlImporter.import_data(path)
 
         if report == "simples":
             return SimpleReport.generate(report_data)
