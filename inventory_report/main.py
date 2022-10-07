b748f444edeb3aae5b268a0248285b6ec179ad14
itaji-create <itaji.1999@gmail.com>
2022-10-07 13:10:03 -0300

    feat: Essa função deve, ao receber pela linha de comando o caminho de um arquivo e o tipo de relatório, devolver o relatório correto

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,18 @@
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+
 def main():
-    pass
+    _, path, type = sys.argv
+
+    if '.csv' in sys.argv[1]:
+        data_type = InventoryRefactor(CsvImporter)
+    if '.json' in sys.argv[1]:
+        data_type = InventoryRefactor(JsonImporter)
+    if '.xml' in sys.argv[1]:
+        data_type = InventoryRefactor(XmlImporter)
+
+    return sys.stdout.write(data_type.import_data(path, type))
